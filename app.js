44f0b6ed7ad7413a6c691179df5464fb3ed27607
asifsultan <muhmmadasif@gmail.com>
2016-11-19 17:49:50 +0500

    minor changes

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+(function(){
+'use strict';
 
+angular.module('NameCalculator',[])
+.controller('NameCalculatorController', function($scope){
+  $scope.name= "";
+  $scope.totalValue=0;
 
+  $scope.displayNumeric = function(){
+    var totalNameValue =calculatNumericForString($scope.name);
+    $scope.totalValue= totalNameValue;
+  }
 
+  function calculatNumericForString(str){
+    var totalStringValue = 0;
+    for( var i =0; i < str.length; i++){
+      totalStringValue = str.charCodeAt(i);
+    }
 
+    return totalStringValue;
 
+  }
 
+});
 
+})();
