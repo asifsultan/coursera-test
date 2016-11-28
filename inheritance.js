a4d2ecedfbb5a9156716115dd0829263214e181b
asifsultan <muhmmadasif@gmail.com>
2016-11-28 21:59:38 +0500

    minor changes

--- inheritance.js
+++ inheritance.js
@@ -16,17 +16,35 @@
 
     ChildController1.$inject =['$scope'];
     function ChildController1($scope){
-        console.log("$scope.parentValue: ", $scope.parentValue);
-        console.log("CHILD scope: ", $scope);
+    //     console.log("$scope.parentValue: ", $scope.parentValue);
+    //     console.log("CHILD scope: ", $scope);
+  
+        // $scope.parentValue = 5;
+        // console.log("*** Changed: $scope.parentValue = 5 ***" );
+        // console.log("$scope.parentValue: ", $scope.parentValue);
+        // console.log($scope);
+
+        // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+        // $scope.pc.parentValue = 5;
+        // console.log("*** Changed: $scope.pc.parentValue: 5 *** ", $scope.pc.parentValue);
+        // console.log("$scope: ", $scope);
+
+        // console.log("$scope.$parent.parentValue: ", $scope.$parent.parentValue);
+        // console.log("$scope.$parent.pc.parentValue: ", $scope.$parent.pc.parentValue);
     }
 
-    ParentController2.$inject =['$scope'];
-    function ParentController2($scope){
+
+    function ParentController2(){
+        var parent =this;
+        parent.value =1;
 
     }
 
     ChildController2.$inject =['$scope'];
     function ChildController2($scope){
+        var child = this;
+        child.value=5;
+        console.log("ChildController2 $scope: ", $scope)
 
     }
 })();
